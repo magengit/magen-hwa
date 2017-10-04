e56ec351e3c01c5bf6c3c9cc24b729873101e031
Reinaldo Penno <rapenno@gmail.com>
2017-10-04 12:19:29 -0700

    Upping magen-rest version
    magen_rest_service==1.2a1

--- hwa/setup.py
+++ hwa/setup.py
@@ -29,7 +29,7 @@ setup(
         'magen_id_client==1.0a1',
         'magen_logger==1.0a1',
         'magen_mongo==1.0a1',
-        'magen_rest_service==1.0a1',
+        'magen_rest_service==1.2a1',
         'magen_statistics_service==1.0a1',
         'magen_test_utils==1.0a1',
         'magen_utils==1.0a1'
